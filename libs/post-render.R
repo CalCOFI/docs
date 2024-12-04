99cc69de813bc57fc6e1f3043e10dfb17b5cb173
Ben Best <ben@ecoquants.com>
2024-12-03 16:04:26 -0800

    ah keep-md for mmd; rm in post-render

---
+++ libs/post-render.R
@@ -0,0 +1,9 @@
+# cleanup because we need this _quarto.yml directive for creating both docx and pdf:
+#   keep-md: true
+#  per [Supporting file are cleanup to soon when using multiformat](https://github.com/quarto-dev/quarto-cli/issues/8373#issuecomment-1979245883)
+
+mds <- list.files(pattern = ".+\\.docx\\.md", recursive = T)
+unlink(mds)
+
+dirs <- list.files(pattern = ".+_files|site_libs")
+unlink(dirs, recursive = TRUE)
